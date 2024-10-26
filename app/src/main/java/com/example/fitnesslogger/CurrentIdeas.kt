c91718a0dc6798e6d193d7a95297d9a948949215
teddy <taddy.blah.blah@gmail.com>
2024-10-26 00:28:54 -0700

    added this

---
+++ app/src/main/java/com/example/fitnesslogger/CurrentIdeas.kt
@@ -0,0 +1,12 @@
+package com.example.fitnesslogger
+
+class CurrentIdeas {
+    //potentially setup navitgation to navigate through views?
+    //think about this more
+    //storing of exercise names - can use string resources
+
+    //potentially use a system like the band database
+
+
+
+}
