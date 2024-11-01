d409b90c9486b31cf5b134e295e090a60e8f248c
teddy <taddy.blah.blah@gmail.com>
2024-11-01 02:08:30 -0700

    reformatted structure

---
+++ app/src/main/java/com/example/fitnesslogger/data/Repositories/ExerciseSetRepository.kt
@@ -0,0 +1,4 @@
+package com.example.fitnesslogger.data.Repositories
+
+class ExerciseSetRepository {
+}
--- app/src/main/java/com/example/fitnesslogger/data/db/Repository/ExerciseRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.example.fitnesslogger.data.db.Repository
-
-class ExerciseRepository {
-}
